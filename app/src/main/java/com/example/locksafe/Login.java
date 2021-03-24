8de4149e4a1649bacbf2252dc49b5e948a06b977
Sinead Cowhig <39123264+Sinead-Cowhig@users.noreply.github.com>
2021-03-24 13:51:01 +0000

    Update Login.java

--- app/src/main/java/com/example/locksafe/Login.java
+++ app/src/main/java/com/example/locksafe/Login.java
@@ -15,12 +15,14 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
     private NestedScrollView nestscroll;
+    private TextInputLayout txtIL_email;
     private TextInputEditText txt_email;
+    private TextInputLayout txtIL_password;
     private  TextInputEditText txt_password;
-
     private AppCompatButton loginbtn;
     private AppCompatTextView register_link;
     private DatabaseHelper dbhelper;
+    private ValidateInput validinput;
 
 
     @Override
@@ -29,27 +31,15 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         nestscroll = (NestedScrollView) findViewById(R.id.nestscroll);
+        txtIL_email = (TextInputLayout) findViewById(R.id.txtIL_email);
         txt_email = (TextInputEditText) findViewById(R.id.txt_email);
+        txtIL_password = (TextInputLayout) findViewById(R.id.txtIL_password);
         txt_password = (TextInputEditText) findViewById(R.id.txt_password);
         loginbtn = (AppCompatButton) findViewById(R.id.loginbtn);
         register_link = (AppCompatTextView) findViewById(R.id.register_link);
         dbhelper = new DatabaseHelper(Login.this);
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(dbhelper.userCheck(txt_email.getText().toString().trim(), txt_password.getText().toString().trim())){
-                    Intent intent = new Intent(Login.this, Encrypt.class);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(getApplicationContext(), " Email Already Exists ", Toast.LENGTH_SHORT).show();
-
-                }
-
-            }
-        });
-
+        validinput = new ValidateInput(Login.this);
+        
         register_link.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -61,7 +51,43 @@ public class Login extends AppCompatActivity {
         });
 
 
+       loginbtn.setOnClickListener(new View.OnClickListener()
+
+            {
+                @Override
+                public void onClick (View v){
+                if (!validinput.isTextFilled(txt_email, txtIL_email, getString(R.string.error_email))) {
+                    return;
+                }
 
 
-    }
-}
+                if (!validinput.isEmail(txt_email, txtIL_email,
+
+                        getString(R.string.error_email))) {
+                    return;
+                }
+
+                if (!validinput.isTextFilled(txt_password, txtIL_password, getString(R.string.error_email))) {
+                    return;
+                }
+
+
+                if (dbhelper.userCheck(txt_email.getText().toString().trim(), txt_password.getText().toString().trim())) {
+                    Intent loginIntent = new Intent(Login.this, Encrypt.class);
+                    loginIntent.putExtra("EMAIL", txt_email.getText().toString().trim());
+                    emptyText();
+                    Login.this.startActivity(loginIntent);
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), " Can't login ", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+
+                private void emptyText () {
+                txt_email.setText(null);
+                txt_password.setText(null);
+            }
+            });
+        }}
